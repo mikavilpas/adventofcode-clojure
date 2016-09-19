8730dc2e717cd6c813c330ea938410229abf8553
Mika Vilpas <mika.vilpas@gmail.com>
2016-09-19 11:50:09 +0300

    Solve day12 part2

--- src/adventofcodeclojure/day12.clj
+++ src/adventofcodeclojure/day12.clj
@@ -20,6 +20,7 @@
        get-numbers
        (reduce + 0)))
 
+;; solve part 1
 (comment
   ;; "Elapsed time: 8.481371 msecs"
   ;; 191164
@@ -27,3 +28,24 @@
    (-> (read-input)
        cheshire/decode
        sum-of-numbers)))
+
+(defn map-with-red-value? [obj]
+  (if (map? obj)
+    (->> obj
+         vals
+         (some (partial = "red")))))
+
+(defn remove-red-maps [edn-object]
+  (walk/prewalk #(if-not (map-with-red-value? %)
+                   %)
+                edn-object))
+
+;; solve part 2
+(comment
+  ;; "Elapsed time: 9.275438 msecs"
+  ;; 87842
+  (time
+   (->> (read-input)
+        cheshire/decode
+        remove-red-maps
+        sum-of-numbers)))
--- test/adventofcodeclojure/day12_test.clj
+++ test/adventofcodeclojure/day12_test.clj
@@ -8,3 +8,12 @@
   (is (= [1 2 3] (day/get-numbers {:a 1 :b 2 :c 3})))
 
   (is (= [1 2 3] (day/get-numbers {:a [1] :b [2] :c [3]}))))
+
+(deftest map-with-red-value?-test
+  (is (true? (day/map-with-red-value? {"foo" "bar", "baz" "red"})))
+  (is (nil? (day/map-with-red-value? [3 3 4]))))
+
+(deftest remove-red-maps-test
+  (is (nil? (day/remove-red-maps {"foo" "red"})))
+  (is (= {"foo" nil}
+         (day/remove-red-maps {"foo" {"bar" "red"}}))))
